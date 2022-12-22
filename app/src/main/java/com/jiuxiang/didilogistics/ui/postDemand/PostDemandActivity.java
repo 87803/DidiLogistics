dfb625a9d8d7413f957dd1818a38ba82d881cddd
87803 <878038720@qq.com>
2022-12-22 17:37:26 +0800

    补充文件

--- app/src/main/java/com/jiuxiang/didilogistics/ui/postDemand/PostDemandActivity.java
+++ app/src/main/java/com/jiuxiang/didilogistics/ui/postDemand/PostDemandActivity.java
@@ -7,18 +7,27 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.alibaba.fastjson.JSONObject;
 import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
+import com.amap.api.services.core.AMapException;
+import com.amap.api.services.core.LatLonPoint;
+import com.amap.api.services.route.DistanceResult;
+import com.amap.api.services.route.DistanceSearch;
 import com.jiuxiang.didilogistics.BR;
 import com.jiuxiang.didilogistics.R;
 import com.jiuxiang.didilogistics.databinding.ActivityPostDemandBinding;
+import com.jiuxiang.didilogistics.utils.HTTPResult;
+import com.jiuxiang.didilogistics.utils.HTTPUtils;
+import com.jiuxiang.didilogistics.utils.PriceUtils;
 import com.lljjcoder.Interface.OnCityItemClickListener;
 import com.lljjcoder.bean.CityBean;
 import com.lljjcoder.bean.DistrictBean;
@@ -27,8 +36,10 @@ import com.lljjcoder.citywheel.CityConfig;
 import com.lljjcoder.style.citylist.Toast.ToastUtils;
 import com.lljjcoder.style.citypickerview.CityPickerView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 public class PostDemandActivity extends AppCompatActivity {
@@ -36,11 +47,12 @@ public class PostDemandActivity extends AppCompatActivity {
     private ActivityPostDemandBinding binding;
     private Calendar calendar;// 用来装日期的
     private DatePickerDialog dialog;
-    CityPickerView mPicker = new CityPickerView();
+    private CityPickerView mPicker = new CityPickerView();
     //声明mlocationClient对象
-    public AMapLocationClient mlocationClient;
+    private AMapLocationClient mlocationClient;
     //声明mLocationOption对象
-    public AMapLocationClientOption mLocationOption = null;
+    private AMapLocationClientOption mLocationOption = null;
+    private DistanceSearch distanceSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +68,25 @@ public class PostDemandActivity extends AppCompatActivity {
 //        binding.setVariable(BR.postDemand, postDemand);
         binding.setLifecycleOwner(this);
 
+        try {
+            distanceSearch = new DistanceSearch(this);
+            distanceSearch.setDistanceSearchListener(new DistanceSearch.OnDistanceSearchListener() {
+                @Override
+                public void onDistanceSearched(DistanceResult distanceResult, int i) {
+                    System.out.println("distanceResult = " + distanceResult.getDistanceResults().get(0).getDistance());
+                    try {
+                        float distance = distanceResult.getDistanceResults().get(0).getDistance();
+                        double price = PriceUtils.getPrice(Double.parseDouble(postDemandViewModel.getLength()), Math.ceil(distanceResult.getDistanceResults().get(0).getDistance() / 1000));
+                        postDemandViewModel.getDistance().setValue(String.valueOf(distance / 1000));
+                        postDemandViewModel.getLowestPrice().setValue(String.valueOf(price));
+                    } catch (Exception ignored) {
+                    }
+                }
+            });
+//            new DistanceSearch.DistanceQuery().addOrigins(new LatLonPoint());
+        } catch (AMapException e) {
+            e.printStackTrace();
+        }
 
         binding.date.setOnClickListener(v -> {
             calendar = Calendar.getInstance();
@@ -178,5 +209,104 @@ public class PostDemandActivity extends AppCompatActivity {
             //显示
             mPicker.showCityPicker();
         });
+
+        binding.startDetailPlace.setOnFocusChangeListener((view, b) -> {
+            if (!b) {
+                if (!postDemandViewModel.getDesPlaceDetail().getValue().equals("") &&
+                        !postDemandViewModel.getStartPlaceDetail().getValue().equals("") &&
+                        !postDemandViewModel.getLength().equals(""))
+                    getDistance(postDemandViewModel.getStartPlace(), postDemandViewModel.getDesPlace());
+            }
+        });
+        binding.desDetailPlace.setOnFocusChangeListener((view, b) -> {
+            if (!b) {
+                if (!postDemandViewModel.getDesPlaceDetail().getValue().equals("") &&
+                        !postDemandViewModel.getStartPlaceDetail().getValue().equals("") &&
+                        !postDemandViewModel.getLength().equals(""))
+                    getDistance(postDemandViewModel.getStartPlace(), postDemandViewModel.getDesPlace());
+            }
+        });
+        binding.etLength.setOnFocusChangeListener((view, b) -> {
+            if (!b) {
+                if (!postDemandViewModel.getDesPlaceDetail().getValue().equals("") &&
+                        !postDemandViewModel.getStartPlaceDetail().getValue().equals("") &&
+                        !postDemandViewModel.getLength().equals(""))
+                    getDistance(postDemandViewModel.getStartPlace(), postDemandViewModel.getDesPlace());
+            }
+        });
+        binding.ivPriceHelp.setOnClickListener((v) -> {
+            new AlertDialog.Builder(this)
+                    .setTitle("价格说明")
+                    .setMessage("系统建议价格是系统根据两地距离以及货物的重量和体积，" +
+                            "选取合适的车型并根据当前市场均价计算得出，" +
+                            "您提供的预期价格不得低于此价格，价格越高，接单几率越大。" +
+                            "\n--------------------------------------\n当前价格计算规则：\n" +
+                            "车型：" + PriceUtils.ITEM[0].length + "米货车，起步价：" + PriceUtils.ITEM[0].flagFallPrice + "元(" + PriceUtils.ITEM[0].flagFallPrice_distance + "公里)，超出后每公里：" + PriceUtils.ITEM[0].price + "元\n" +
+                            "车型：" + PriceUtils.ITEM[1].length + "米货车，起步价：" + PriceUtils.ITEM[1].flagFallPrice + "元(" + PriceUtils.ITEM[1].flagFallPrice_distance + "公里)，超出后每公里：" + PriceUtils.ITEM[1].price + "元\n" +
+                            "车型：" + PriceUtils.ITEM[2].length + "米货车，起步价：" + PriceUtils.ITEM[2].flagFallPrice + "元(" + PriceUtils.ITEM[2].flagFallPrice_distance + "公里)，超出后每公里：" + PriceUtils.ITEM[2].price + "元\n" +
+                            "车型：" + PriceUtils.ITEM[3].length + "米货车，起步价：" + PriceUtils.ITEM[3].flagFallPrice + "元(" + PriceUtils.ITEM[3].flagFallPrice_distance + "公里)，超出后每公里：" + PriceUtils.ITEM[3].price + "元\n" +
+                            "车型：" + PriceUtils.ITEM[4].length + "米货车，起步价：" + PriceUtils.ITEM[4].flagFallPrice + "元(" + PriceUtils.ITEM[4].flagFallPrice_distance + "公里)，超出后每公里：" + PriceUtils.ITEM[4].price + "元\n" +
+                            "--------------------------------------\n您可以在车主接单前，与车主协商最终的价格，并在平台上修改价格，接单后，不支持修改价格，" +
+                            "为保证订单安全，请勿在平台外交易，如有疑问，请联系客服。")
+                    .setPositiveButton("我知道了", null)
+                    .show();
+        });
+    }
+
+    private void getDistance(String start, String des) {
+        postDemandViewModel.getDistance().setValue("计算中...");
+        postDemandViewModel.getLowestPrice().setValue("计算中...");
+        List<LatLonPoint> startPoints = new ArrayList<>();
+        final LatLonPoint[] desPoints = {null};
+
+        //将起点和终点的地址转换为经纬度
+        HTTPUtils.get_api("https://restapi.amap.com/v3/geocode/geo?address=" + start + "&key=", new HTTPResult() {
+            @Override
+            public void onSuccess(JSONObject result) {
+                if (result.getString("status").equals("1")) {
+                    JSONObject geocodes = result.getJSONArray("geocodes").getJSONObject(0);
+                    String location = geocodes.getString("location");
+                    String[] split = location.split(",");
+                    startPoints.add(new LatLonPoint(Double.parseDouble(split[1]), Double.parseDouble(split[0])));
+
+                    HTTPUtils.get_api("https://restapi.amap.com/v3/geocode/geo?address=" + des + "&key=", new HTTPResult() {
+                        @Override
+                        public void onSuccess(JSONObject result) {
+                            if (result.getString("status").equals("1")) {
+                                JSONObject geocodes = result.getJSONArray("geocodes").getJSONObject(0);
+                                String location = geocodes.getString("location");
+                                String[] split = location.split(",");
+                                desPoints[0] = (new LatLonPoint(Double.parseDouble(split[1]), Double.parseDouble(split[0])));
+
+
+                                //计算两点之间的距离
+                                DistanceSearch.DistanceQuery distanceQuery = new DistanceSearch.DistanceQuery();
+                                distanceQuery.setOrigins(startPoints);
+                                distanceQuery.setDestination(desPoints[0]);
+//设置测量方式，支持直线和驾车
+                                distanceQuery.setType(DistanceSearch.TYPE_DRIVING_DISTANCE);
+                                distanceSearch.calculateRouteDistanceAsyn(distanceQuery);
+                            } else {
+                                ToastUtils.showLongToast(PostDemandActivity.this, "起始地点经纬度获取失败");
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(String error) {
+                            Toast.makeText(PostDemandActivity.this, error, Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                } else {
+                    ToastUtils.showLongToast(PostDemandActivity.this, "起始地点经纬度获取失败");
+                }
+            }
+
+            @Override
+            public void onFailure(String error) {
+                Toast.makeText(PostDemandActivity.this, error, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
